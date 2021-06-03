ba2c6bd8a87721e80ce629026a82cae7f61153be
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2021-06-03 11:30:40 +0200

    android.preference.PreferenceManager import changed to androidx.preference.PreferenceManager

--- app/src/main/java/com/example/growbro/MainActivity.java
+++ app/src/main/java/com/example/growbro/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.growbro;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
